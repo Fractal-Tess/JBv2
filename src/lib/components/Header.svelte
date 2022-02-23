0c51e1ba4babbb29764e9f88fd1028fb35b1c5f4
Fractal-tess <vgfractal@gmail.com>
2022-02-23 12:35:09 +0200

    first commit

---
+++ src/lib/components/Header.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+</script>
+
+<nav>
+	<h1>Hello world</h1>
+</nav>
+
+<nav>
+	<h1>Hello world</h1>
+</nav>
+
+<style lang="postcss">
+	nav {
+		@apply bg-red-400 dark:bg-black;
+	}
+</style>
